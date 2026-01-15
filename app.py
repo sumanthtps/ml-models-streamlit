2b1bcd2c622b74211deb778bd3f211ea9af50427
Sumanth T P <sumanthtps@gmail.com>
2026-01-15 23:22:13 +0530

    feat: [0001] "Setup"

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="ML Assignment 2", layout="wide")
+st.title("ML Assignment 2 - Setup Check")
+st.write("If you can see this page, Streamlit is working.")
